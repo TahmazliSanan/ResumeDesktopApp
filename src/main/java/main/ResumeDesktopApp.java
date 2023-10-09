cd3dcfd1b054648904ead326b65849095d6f5fb2
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-10-09 14:57:17 +0400

    Create desktop app

---
+++ src/main/java/main/ResumeDesktopApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author user
+ */
+public class ResumeDesktopApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
